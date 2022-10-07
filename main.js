907f78a15044431edb6d73389369729ab65bdd7a
Justin Sweeten <jtsweeten@gmail.com>
2022-10-07 09:42:05 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,92 @@
+// *** How to grill chicken ***
+
+const buyChicken = () => {
+    console.log("Buy chicken")
+}
+
+const cleanChicken = () => {
+    console.log("Clean chicken")
+}
+
+const marinateChicken = () => {
+    console.log("Marinate Chicken")
+}
+
+const letMarinate = () => {
+    console.log("Let chicken marinate for 24 hours")
+}
+
+const buyCharcoal = () => {
+    console.log("Buy charcoal")
+}
+
+const gatherKindling = () => {
+    console.log("Gather kindling")
+}
+
+const prepKindling = () => {
+    console.log("Place kindling in bottom of charcoal chimney")
+}
+
+const prepCharcoal = () => {
+    console.log("Dump charcoal in top of charcoal chimney")
+}
+
+const gatherChimney = () => {
+    console.log("Check to make sure you have a charcoal chimney")
+}
+
+const gatherGrill = () => {
+    console.log("Bring charcoal grill out from storage shed")
+}
+
+const igniteKindling = () => {
+    console.log("Ignite kindling")
+}
+
+const dumpChimney = () => {
+    console.log("When charcoal is lit, dump charcoal into charcoal grill")
+}
+
+const replaceGrilltop = () => {
+    console.log("Take grilltop off the grill to allow access to charcoal pit")
+}
+
+const removeGrilltop = () => {
+    console.log("When charcoal is in charcoal pit, place grill top back on")
+}
+
+const grillChicken = () => {
+    console.log("Place marinated chicken on grill")
+}
+
+const flipChicken = () => {
+    console.log("When chicken starts to turn white, flip over")
+}
+
+const checkTemp = () => {
+    console.log("Check every 3 minutes to see if chicken is white throughout")
+}
+
+const removeChicken = () => {
+    console.log("When chicken turns white throughout, remove chicken from heat")
+}
+
+buyChicken()
+buyCharcoal()
+cleanChicken()
+marinateChicken()
+letMarinate()
+gatherGrill()
+gatherChimney()
+gatherKindling()
+removeGrilltop()
+prepKindling()
+prepCharcoal()
+igniteKindling()
+dumpChimney()
+replaceGrilltop()
+grillChicken()
+flipChicken()
+checkTemp()
+removeChicken()
